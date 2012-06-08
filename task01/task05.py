0c8eb029bb35fe4d09863c1bcb78191ce094285e
Dmitry Sandalov <dmitry@sandalov.org>
2012-06-08 15:45:59 +0400

    Task 5

--- task01/task05.py
+++ task01/task05.py
@@ -1,15 +1,13 @@
 def removeZeroesAfterMax(_list):
     maxElementIndex = _list.index(max(_list))
-    while(_list.count(0)!=0):
+    finished = False
+    while not finished:
         for i in xrange(maxElementIndex, len(_list)):
             if (_list[i]==0):
                 _list.pop(i)
-                break
-    
-    _list.pop(5)
-    print _list
-             
-    return "\r"
+                break            
+        if (i==len(_list)-1):
+            finished = True             
+    return _list
 a = [1, 2, 3, 4, 100, 0, 0, 0, 6]
-#a= [0, 1, 2, 3,   4, 5, 6, 7, 8]
 print removeZeroesAfterMax(a)
